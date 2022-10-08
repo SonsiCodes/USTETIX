630cf4a067d7569e1d437d577efecf484699857f
NetAcad DEVASC <changeme@example.com>
2022-10-08 14:16:58 +0000

    lat

--- LabActivity3.py
+++ LabActivity3.py
@@ -25,6 +25,9 @@ print("with the Autonomous System Number: " + asn[0:6])
 
 # getting location info 
 country = response["country"]
+lat = str(response["lat"])
+lon = str(response["lon"])
+GeoLocation = " The latitude is " + lat + " and longitude " + lon
 countryCode = response["countryCode"]
 region = response["region"]
 city = response["city"]
@@ -32,7 +35,7 @@ zipcode = response["zip"]
 
 #concatenating location info
 CompleteLoc = ("The IP address in located in: " + country + "\n with the country code: " + countryCode 
-+ "\n in region: " + region + "\n in the city of: " + city + "\n with the zip code: " + zipcode)
++ "\n in region: " + region + "\n in the city of: " + city + "\n with the zip code: " + zipcode + "\n Geolocation:"+ GeoLocation)
 print(CompleteLoc)
 
 
